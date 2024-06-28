8e225904afbd6d69740c2486ebe408b8803a72b2
Richasy <Thansy@foxmail.com>
2024-06-28 20:31:42 +0800

    内置文心一言 4.0 Turbo 模型

--- src/Core/RodelChat.Core/Providers/Predefined/Qianfan.cs
+++ src/Core/RodelChat.Core/Providers/Predefined/Qianfan.cs
@@ -11,33 +11,39 @@ internal static partial class PredefinedModels
     {
         new ChatModel
         {
+            DisplayName = "ERNIE-4.0-Turbo-8K",
+            Id = "ernie-4.0-turbo-8k",
+            ContextLength = 8124,
+        },
+        new ChatModel
+        {
             DisplayName = "ERNIE-4.0-8K",
             Id = "completions_pro",
-            ContextLength = 5000,
+            ContextLength = 8124,
         },
         new ChatModel
         {
             DisplayName = "ERNIE-3.5-8K",
             Id = "completions",
-            ContextLength = 4000,
+            ContextLength = 8124,
         },
         new ChatModel
         {
             DisplayName = "ERNIE-Speed-8K",
             Id = "ernie_speed",
-            ContextLength = 4000,
+            ContextLength = 8124,
         },
         new ChatModel
         {
             DisplayName = "ERNIE-Lite-8K",
             Id = "eb-instant",
-            ContextLength = 4000,
+            ContextLength = 8124,
         },
         new ChatModel
         {
             DisplayName = "ERNIE-Tiny-8K",
             Id = "ernie-tiny-8k",
-            ContextLength = 4000,
+            ContextLength = 8124,
         },
     };
 }
