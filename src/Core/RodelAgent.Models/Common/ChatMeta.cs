664594e3a9fe990ce4a8886c49a8016bed85c863
Richasy <Thansy@foxmail.com>
2025-02-26 21:05:39 +0800

    🔖 submission: 移除ChatDbContext及其迁移工具
    移除ChatDbContext和MigrationUtils文件
    删除与ChatDbContext相关的迁移文件
    在ChatMeta.cs中添加ChatConversationMeta和ChatGroupMeta类
    简化数据库上下文管理，优化代码结构
    涉及文件：ChatDbContext.cs, MigrationUtils.cs, ChatMeta.cs

---
+++ src/Core/RodelAgent.Models/Common/ChatMeta.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Richasy. All rights reserved.
+
+namespace RodelAgent.Models.Common;
+
+/// <summary>
+/// 对话元数据.
+/// </summary>
+public class ChatConversationMeta : Metadata;
+
+/// <summary>
+/// 群组元数据.
+/// </summary>
+public class ChatGroupMeta : Metadata;
